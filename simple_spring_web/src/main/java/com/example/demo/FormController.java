55a278538ab029edfa23d9dffbaa0a18af2ad718
naveenrc10 <nav.chakravarthy@gmail.com>
2025-02-11 13:21:42 +0530

    fff

--- simple_spring_web/src/main/java/com/example/demo/FormController.java
+++ simple_spring_web/src/main/java/com/example/demo/FormController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
