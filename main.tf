2f20a6fec944d3c5b26b885c66c6daf6c7b8fe62
naveenrc10 <nav.chakravarthy@gmail.com>
2025-02-12 17:03:32 +0530

    fff

--- main.tf
+++ main.tf
@@ -25,6 +25,13 @@ resource "aws_security_group" "DC_SG" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        description = "Allow HTTP"
+        from_port   = 2377
+        to_port     = 2377
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress { 
         from_port = 0
         to_port = 0
